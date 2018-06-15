4699f535b642ec30fb0ebaf1af89facb01b554ac
Cyril Delabarre <cyril.delabarre@hotmail.com>
2018-06-15 22:57:24 +0200

    quick fix

--- .tmp/scripts/main-index.bundle.js
+++ .tmp/scripts/main-index.bundle.js
@@ -8,7 +8,35 @@ $('.slider-thumb').slick({
   dots: false,
   arrows: false,
   centerMode: false,
-  focusOnSelect: true
+  focusOnSelect: true,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      autoplay: true,
+      dots: false
+    }
+  }, {
+    breakpoint: 600,
+    settings: {
+      autoplay: true,
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  }, {
+    breakpoint: 480,
+    settings: {
+      autoplay: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  }]
+
 });
 
 // COUNTDOWN 
